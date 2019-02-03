bf67316dfc23f486459b6c09da519ceec4c4e15c
bdschoeler <bdschoeler@gmail.com>
2019-02-03 02:01:16 -0500

    Added post support

--- client.py
+++ client.py
@@ -1,19 +1,20 @@
 import socket
 from queryBuilder import QueryBuilder
-from urllib.parse import urlparse
 
 class Client:
 
-    def get(self,url,verbose):
+    def get(self,url,verbose,headers):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        parsedUrl = urlparse(url)
+        parsedUrl = url.split('/')
 
-        s.connect('www.'+str(parsedUrl.netloc), 80)
+        s.connect('www.'+parsedUrl[2], 80)
 
-        queryParams = str(parsedUrl.path) +'?'+ str(parsedUrl.query)
+        queryParams = parsedUrl[3]
         query = QueryBuilder('GET', queryParams)
-        query.addHeader('Host',str(parsedUrl.netloc))
+        query.addHeader('Host',parsedUrl[2])
         query.addHeader('User-Agent', '445Assignment1')
+        for pair in headers:
+            query.addHeader(pair[0], pair[1])
         
         s.sendall(bytes(query.getString(), 'utf8'))
 
@@ -23,8 +24,30 @@ class Client:
         else:
             recBytesSplit = recBytes.split(b'\r\n\r\n')
             print("".join(map(chr,recBytesSplit[1])))
-            
+
         s.close()
 
+    def post(self,url,verbose, body, headers):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        parsedUrl = url.split('/')
 
-    def post(self):
+        s.connect('www.'+parsedUrl[2], 80)
+
+        queryParams = parsedUrl[3]
+        query = QueryBuilder('POST', queryParams)
+        query.addHeader('Host',parsedUrl[2])
+        query.addHeader('User-Agent', '445Assignment1')
+        query.addHeader('Body', body)
+        for pair in headers:
+            query.addHeader(pair[0], pair[1])
+        
+        s.sendall(bytes(query.getString(), 'utf8'))
+
+        recBytes = s.recv(4096)
+        if(verbose):
+            print("".join(map(chr,recBytes)))
+        else:
+            recBytesSplit = recBytes.split(b'\r\n\r\n')
+            print("".join(map(chr,recBytesSplit[1])))
+
+        s.close()
