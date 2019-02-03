3e9d52bd9ea5db5052e68a61e3da4a50c33907c2
bdschoeler <bdschoeler@gmail.com>
2019-02-02 23:13:25 -0500

    Finished GET

--- client.py
+++ client.py
@@ -4,21 +4,26 @@ from urllib.parse import urlparse
 
 class Client:
 
-    def get(self,url):
+    def get(self,url,verbose):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         parsedUrl = urlparse(url)
 
         s.connect('www.'+str(parsedUrl.netloc), 80)
 
         queryParams = str(parsedUrl.path) +'?'+ str(parsedUrl.query)
-
         query = QueryBuilder('GET', queryParams)
         query.addHeader('Host',str(parsedUrl.netloc))
         query.addHeader('User-Agent', '445Assignment1')
         
         s.sendall(bytes(query.getString(), 'utf8'))
+
         recBytes = s.recv(4096)
-        print("".join(map(chr, recBytes)))
+        if(verbose):
+            print("".join(map(chr,recBytes)))
+        else:
+            recBytesSplit = recBytes.split(b'\r\n\r\n')
+            print("".join(map(chr,recBytesSplit[1])))
+            
         s.close()
 
 
